94a1f8e6bf57e2c144ac8884ad3c77549f1d1298
Qian JX <raist.qian@gmail.com>
2016-09-12 15:50:10 +0800

    Initial Commit

---
+++ TouchIDDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchIDDemo2
+//
+//  Created by Qian JX on 16/9/12.
+//  Copyright © 2016年 zfsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchIDDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchIDDemo2
+//
+//  Created by Qian JX on 16/9/12.
+//  Copyright © 2016年 zfsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
